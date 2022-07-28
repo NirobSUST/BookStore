d475e00929c5a985932094aff2e5fbba6fb267a5
Shahadat_Nirob <shahadat.sust.math@gmail.com>
2022-07-28 20:00:13 +0600

    MVC Basic Done

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -1,5 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+startup.Configure(app, builder.Environment);
+
+/*
+var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
@@ -9,7 +18,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -19,3 +32,4 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+*/
---
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Models;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where( x => x.Title.Contains(title) && x.Author.Contains(authorName)).ToList();
+        }
+
+
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel() { Id = 1, Title = "C" , Author = "Subeen" },
+                new BookModel() { Id = 2, Title = "C++" , Author = "Shahadat" },
+                new BookModel() { Id = 3, Title = "Java" , Author = "Nirob" },
+                new BookModel() { Id = 4, Title = "Python" , Author = "Neal" },
+                new BookModel() { Id = 5, Title = "C#" , Author = "Heisenberg" }
+            };
+        }
+    }
+}
